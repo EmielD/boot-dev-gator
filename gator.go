c9c4cb3e457de4002d2dbfc2d03d0292010846d2
EmielD <ej.dorsman@gmail.com>
2025-01-09 23:11:40 +0100

    added config struct with read/setuser functionality for config json file

--- gator.go
+++ gator.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/emield/gator/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	config, err := config.Read()
+	if err != nil {
+		fmt.Printf("Something went wrong reading the config: %v\n", err)
+	}
+
+	config, err = config.SetUser("Emiel")
+	if err != nil {
+		fmt.Printf("Something went wrong setting the user: %v\n", err)
+	}
+
+	fmt.Printf("%s\n", config)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Db_url            string `json:"db_url"`
+	Current_user_name string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, fmt.Errorf("could not get users home directory: %v", err)
+	}
+
+	dat, err := os.ReadFile(userHomeDir + "/.gatorconfig.json")
+	if err != nil {
+		return Config{}, fmt.Errorf("error reading file: %v", err)
+	}
+
+	var config Config
+	if err := json.Unmarshal(dat, &config); err != nil {
+		return Config{}, fmt.Errorf("could not decode configuration file: %v", err)
+	}
+
+	return config, nil
+}
+
+func (c *Config) SetUser(name string) (Config, error) {
+	c.Current_user_name = name
+
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		return Config{}, fmt.Errorf("could not get users home directory: %v", err)
+	}
+
+	data, err := json.Marshal(c)
+	if err != nil {
+		return Config{}, fmt.Errorf("could not encode user: %v", err)
+	}
+
+	err = os.WriteFile(userHomeDir+"/.gatorconfig.json", data, 0644)
+	if err != nil {
+		return Config{}, fmt.Errorf("error reading file: %v", err)
+	}
+
+	config := c
+	return *config, nil
+
+}
